0d3e7899489a1f57d595c9c405ff7e83a5a92d04
Razvan Dorcioman <34245071+Razvan-Dorcioman@users.noreply.github.com>
2021-11-06 15:21:12 +0200

    add test controller

---
+++ Smarthack2021/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Smarthack2021.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello from backend";
+        }
+    }
+}
