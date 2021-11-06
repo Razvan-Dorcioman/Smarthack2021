a7c1c9bc214076df4208e045c65bad88be1d81c0
Razvan Dorcioman <34245071+Razvan-Dorcioman@users.noreply.github.com>
2021-11-06 11:41:41 +0200

    first commit

--- Smarthack2021/Startup.cs
+++ Smarthack2021/Startup.cs
@@ -26,11 +26,10 @@ namespace Smarthack2021
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Smarthack2021", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo {Title = "Smarthack2021", Version = "v1"});
             });
         }
 
@@ -50,10 +49,7 @@ namespace Smarthack2021
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
 }
